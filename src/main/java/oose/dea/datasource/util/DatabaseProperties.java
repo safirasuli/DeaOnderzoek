02de0379e6e8bf55726e13b162632ad1d9151de0
Safi <safi_rasuli@hotmail.com>
2018-03-24 15:47:57 +0100

    properties load function

--- src/main/java/oose/dea/datasource/util/DatabaseProperties.java
+++ src/main/java/oose/dea/datasource/util/DatabaseProperties.java
@@ -1,4 +1,51 @@
 package oose.dea.datasource.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DatabaseProperties {
+
+    private Properties properties;
+    private static final  String PROPERTYNAME = "database.properties";
+    private static final String CONNECTIONURL = "connectionstring";
+    private static final String DRIVER = "driver";
+    public DatabaseProperties() {
+        this.properties = new Properties();
+        loadPropertyFile();
+    }
+
+
+    private void loadPropertyFile(){
+
+        InputStream stream = null;
+        try{
+            stream = new FileInputStream(PROPERTYNAME);
+            properties.load(stream);
+
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if(stream != null){
+                try {
+                    stream.close();
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+    public String getConnectionUrl(){
+        return properties.getProperty(CONNECTIONURL);
+    }
+
+    public String getDriver(){
+        return properties.getProperty(DRIVER);
+    }
+
+
 }
